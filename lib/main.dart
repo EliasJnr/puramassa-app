d8d24f8c8c7db735910934456d200bc829629ce6
elias <araujo>
2019-09-24 15:37:54 -0400

    firebase messaging

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/models/cart_model.dart';
 import 'package:loja_virtual/screens/home_screen.dart';
+import 'package:loja_virtual/utils/firebase_notification_handler.dart';
 import 'package:scoped_model/scoped_model.dart';
-
 import 'models/user_model.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
+  MyApp() {
+    new FirebaseNotifications().setUpFirebase();
+  }
 
   @override
   Widget build(BuildContext context) {
